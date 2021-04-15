188313bea34640d9ae48ea8d44643eb7af4f3d87
Ashish Chander <ashishedu98@gmail.com>
2021-04-15 21:46:19 +0530

    changes leftover

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './components/navbar/navbar.js';
+import {BrowserRouter,Route} from 'react-router-dom';
+import HomePage from './components/home/home.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavigationBar/>
+        <Route path="/home" component={HomePage} exact />
+        </BrowserRouter>
     </div>
   );
 }
